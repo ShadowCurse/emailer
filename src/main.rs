38bb6681012b0f1635f442b7b11217c723fa02d9
ShadowCurse <shadowcurse57@gmail.com>
2022-03-22 02:01:04 +0300

    refactor startup into AppServer

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,11 @@
-use emailer::{
-    config::read_config, email_client::EmailClient, startup::run, telemetry::init_logging,
-};
-use sqlx::PgPool;
+use emailer::{config::read_config, startup::AppServer, telemetry::init_logging};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     init_logging("emailer", "info", std::io::stdout);
 
     let config = read_config().expect("Failed to read config");
-    let connection = config.database.with_db();
-    let connection_pool = PgPool::connect_with(connection)
-        .await
-        .expect("Failed to connect to Postgres");
-    let sender = config.email_client.sender().expect("Invalid sender email");
-    let email_client = EmailClient::new(
-        config.email_client.base_url,
-        sender,
-        config.email_client.auth_token,
-        std::time::Duration::from_secs(5),
-    );
-
-    let listener =
-        std::net::TcpListener::bind(config.application.address()).expect("Unable to bind port");
-    run(listener, connection_pool, email_client)?.await
+    let server = AppServer::build(config).await?;
+    server.run().await?;
+    Ok(())
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,3 +1,4 @@
+use crate::config::Config;
 use crate::{email_client::EmailClient, routes::*};
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
@@ -5,22 +6,57 @@ use sqlx::PgPool;
 use std::net::TcpListener;
 use tracing_actix_web::TracingLogger;
 
-pub fn run(
-    listener: TcpListener,
-    connection_pool: PgPool,
-    email_client: EmailClient,
-) -> Result<Server, std::io::Error> {
-    let connection_pool = web::Data::new(connection_pool);
-    let email_client = web::Data::new(email_client);
-    let server = HttpServer::new(move || {
-        App::new()
-            .wrap(TracingLogger::default())
-            .route("/health_check", web::get().to(health_check))
-            .route("/subscriptions", web::post().to(subscribe))
-            .app_data(connection_pool.clone())
-            .app_data(email_client.clone())
-    })
-    .listen(listener)?
-    .run();
-    Ok(server)
+pub struct AppServer {
+    port: u16,
+    server: Server,
+}
+
+impl AppServer {
+    pub async fn build(config: Config) -> Result<Self, std::io::Error> {
+        let connection = config.database.with_db();
+        let connection_pool = PgPool::connect_with(connection)
+            .await
+            .expect("Failed to connect to Postgres");
+        let sender = config.email_client.sender().expect("Invalid sender email");
+        let email_client = EmailClient::new(
+            config.email_client.base_url,
+            sender,
+            config.email_client.auth_token,
+            std::time::Duration::from_secs(5),
+        );
+        let listener =
+            TcpListener::bind(config.application.address()).expect("Unable to bind port");
+        let port = listener.local_addr().unwrap().port();
+        let server = Self::running_server(listener, connection_pool, email_client)?;
+
+        Ok(Self { port, server })
+    }
+
+    pub fn port(&self) -> u16 {
+        self.port
+    }
+
+    pub async fn run(self) -> Result<(), std::io::Error> {
+        self.server.await
+    } 
+
+    fn running_server(
+        listener: TcpListener,
+        connection_pool: PgPool,
+        email_client: EmailClient,
+    ) -> Result<Server, std::io::Error> {
+        let connection_pool = web::Data::new(connection_pool);
+        let email_client = web::Data::new(email_client);
+        let server = HttpServer::new(move || {
+            App::new()
+                .wrap(TracingLogger::default())
+                .route("/health_check", web::get().to(health_check))
+                .route("/subscriptions", web::post().to(subscribe))
+                .app_data(connection_pool.clone())
+                .app_data(email_client.clone())
+        })
+        .listen(listener)?
+        .run();
+        Ok(server)
+    }
 }
